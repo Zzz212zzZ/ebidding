a8194daed39b104300260d7d52ac9130d8c7c3ff
lyy <2991647507@qq.com>
2023-06-27 16:58:09 +0800

    创建模块之前

--- src/main/java/com/ebidding/service/controller/AccountController.java
+++ src/main/java/com/ebidding/service/controller/AccountController.java
@@ -1,18 +1,26 @@
 package com.ebidding.service.controller;
 
+import com.ebidding.service.domain.Account;
+import com.ebidding.service.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/accounts")
 public class AccountController {
+    @Autowired
+    private AccountService accountService;
+
     @GetMapping()
-    public ResponseEntity<List<String>> getAccounts(){
-        return ResponseEntity.ok(new ArrayList<>());
+    // PathVariable v.s. RequestParam v.s. RequestBody
+    // http://localhost:8080/api/v1/accounts?name={inputName}
+    // http://localhost:8080/api/v1/accounts/1
+    // [POST] http://localhost:8080/api/v1/accounts {}
+    public ResponseEntity<Account> getAccount(@RequestParam("name") String name) {
+        return ResponseEntity.ok(this.accountService.findByName(name));
     }
 }
