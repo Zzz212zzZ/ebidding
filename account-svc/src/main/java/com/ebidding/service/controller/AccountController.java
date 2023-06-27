db9d594978fffab653ca14a60015e2456a319488
tyx <tianyx22379@126.com>
2023-06-27 20:02:46 +0800

    6.27进度同步

---
+++ account-svc/src/main/java/com/ebidding/service/controller/AccountController.java
@@ -0,0 +1,23 @@
+package com.ebidding.service.controller;
+
+import com.ebidding.service.domain.Account;
+import com.ebidding.service.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/accounts")
+public class AccountController {
+    @Autowired
+    private AccountService accountService;
+
+    @GetMapping()
+    public ResponseEntity<Account> getAccount(@RequestParam("name") String name) {
+//      Account account= accountService.findByName(name);
+        return ResponseEntity.ok(this.accountService.findByName(name));
+    }
+}
