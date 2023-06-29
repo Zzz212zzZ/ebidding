0b1169a83e93e4862652ac74d23c5478b08191ce
tyx <tianyx22379@126.com>
2023-06-29 16:03:59 +0800

    implement role based authorize

--- bid-svc/src/main/java/com/ebidding/bid/controller/BidController.java
+++ bid-svc/src/main/java/com/ebidding/bid/controller/BidController.java
@@ -2,6 +2,8 @@ package com.ebidding.bid.controller;
 
 import com.ebidding.account.api.AccountDTO;
 import com.ebidding.bid.service.BidService;
+import com.ebidding.common.auth.AuthConstant;
+import com.ebidding.common.auth.Authorize;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,7 @@ public class BidController {
     private BidService bidService;
 
     @GetMapping()
+    @Authorize(AuthConstant.TRADER)
     public ResponseEntity<AccountDTO> getBid(@RequestParam("name") String name) {
         return ResponseEntity.ok(this.bidService.findByName(name));
     }
