22446369b44e280fc0c685054ded2d5758726a1d
Zzz212zzZ <2120020201@qq.com>
2023-07-02 17:00:56 +0800

    集成了nacos，解决了svc-api的jdk版本为5的bug

--- service/bid-svc/src/main/java/com/ebidding/bid/BidServiceApplication.java
+++ service/bid-svc/src/main/java/com/ebidding/bid/BidServiceApplication.java
@@ -2,8 +2,11 @@ package com.ebidding.bid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients(basePackages = {"com.ebidding.account", "com.ebidding.bwic"})
 public class BidServiceApplication {
