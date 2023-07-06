3833ea07abbfbe82f528adedc1ef4be3b08cc1bc
lyy <2991647507@qq.com>
2023-07-06 15:54:33 +0800

    前后端通讯，后端功能的调试，以及仓库整理

---
+++ service/account-svc/src/main/java/com/ebidding/account/domain/Account.java
@@ -0,0 +1,28 @@
+package com.ebidding.account.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+
+public class Account {
+
+    @Id
+    @Column(name = "account_id", nullable = false)
+    private String id;
+
+    private String name;
+    private String password_hash;
+
+    @Enumerated(EnumType.STRING)
+    private AccountRole role;
+
+}
