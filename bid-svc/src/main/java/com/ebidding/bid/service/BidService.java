c81044fead69f5c0809baa6d8b7506b4f890845d
Zzz212zzZ <2120020201@qq.com>
2023-07-07 15:04:31 +0800

    重新开始

---
+++ bid-svc/src/main/java/com/ebidding/bid/service/BidService.java
@@ -0,0 +1,146 @@
+package com.ebidding.bid.service;
+
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import com.ebidding.account.api.AccountDTO;
+import com.ebidding.account.api.AccountClient;
+import com.ebidding.bwic.api.BwicClient;
+import com.ebidding.bid.api.PriceResponseDTO;
+import com.ebidding.bid.domain.Bid;
+import com.ebidding.bid.domain.BidRank;
+import com.ebidding.bid.domain.BidRankPK;
+import com.ebidding.bid.repository.BidRankRepository;
+import com.ebidding.bid.repository.BidRepository;
+import com.ebidding.common.utils.WebSocketMessageUtil;
+import com.ebidding.common.websocket.UserIdSessionManager;
+import com.ebidding.common.websocket.enums.WebSocketMsgType;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+@RequiredArgsConstructor
+//针对标有 @NonNull 注解的变量和 final 变量进行参数的构造方法。
+public class BidService {
+
+    @Autowired
+    private final AccountClient accountClient;
+
+    @Autowired
+    private BwicClient bwicClient;
+
+
+    //    @Autowired
+//    public BidService(AccountClient accountClient) {
+//        this.accountClient = accountClient;
+//    }用@RequiredArgsConstructor代替
+    @Autowired
+    private BidRepository bidRepository;
+
+    @Autowired
+    private BidRankRepository bidRankRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public AccountDTO getByName(String inputName) {//调用account-api
+        return this.accountClient.getAccount(inputName).getBody();
+    }
+
+    public Bid getByBidId(Long bidId) {
+        return this.bidRepository.findByBidId(bidId).orElse(null);
+    }
+
+
+    public Bid createBid(Bid bid) {
+
+
+        // 1. 添加AccountId和BwicId,再添加时间戳
+        BidRank bidRank = new BidRank();
+        BidRankPK embbedeId = new BidRankPK(bid.getAccountId(), bid.getBwicId());
+        bidRank.setId(embbedeId);
+        bidRank.setPrice(bid.getPrice());
+        Timestamp preTime=new Timestamp(System.currentTimeMillis());
+        bidRank.setTime(preTime);
+        bid.setTime(preTime);
+
+        //2.现在获取排名
+        this.bidRankRepository.save(bidRank);
+        Long ranking = this.getRankByBwicIdAndAccountId(bid.getBwicId(),bid.getAccountId());
+
+        // 3. 更新Bid的排名
+        bid.setRanking(ranking);
+        this.bidRepository.save(bid);
+
+        //最后还要更新bwic中的bidCounts，last_bid_time，和present_price(如果比present_price高的话)
+        //这个方法还要更新bond中的transaction_counts
+        bwicClient.updateBwic(bid.getBwicId(),bid.getPrice(),bid.getTime());
+        return bid;
+    }
+
+    public Long getRankByBwicIdAndAccountId(Long bwicId, Long accountId) {
+        // 检查bidRank是否存在
+        Long ranking = bidRankRepository.getRanking(bwicId,accountId);
+       return ranking;
+
+    }
+
+    public Long getParticipantCount(Long bwicId) {
+        return bidRankRepository.countByBwicId(bwicId);
+    }
+
+    public PriceResponseDTO getPrice(Long bwicId, Long accountId) {
+
+        PriceResponseDTO response = new PriceResponseDTO();
+        BidRank bidRank = this.bidRankRepository.findByBwicIdAndAccountId(bwicId, accountId).orElseThrow(()-> new NoSuchElementException("Record not found"));
+        response.setPrice(bidRank.getPrice());
+        Long rank = this.getRankByBwicIdAndAccountId(bwicId, accountId);
+        response.setRanking(rank);
+        if (rank == 1) {
+            response.setRanking(this.bidRankRepository.getRanking(bwicId, accountId));
+            if (this.getParticipantCount(bwicId) > 1) {
+                Double secondPrice = this.bidRankRepository.getSecondHighestPrice(bwicId).orElse(null);
+                response.setSecondPrice(secondPrice);
+            }
+        }
+        return response;
+    }
+
+//    public BidRank getByBidId(Long bidId){
+//        return this.bidRankRepository.findByBidId(bidId).orElse(null);
+//    }
+
+
+    public Bid getSuccesBidByBwicid(Long bwicId) {
+        Bid bid = bidRepository.getSuccesBidByBwicid(bwicId);
+        WebSocketSession webSocketSession = UserIdSessionManager.getSession(bid.getAccountId().intValue());
+        JSONObject msgObj = new JSONObject();
+        msgObj.put("msgType", WebSocketMsgType.NOTICE_RESULT_MSG.getCode());
+        msgObj.put("result", "success");
+        msgObj.put("msg", "恭喜您，此次拍卖竞拍成功！");
+        WebSocketMessageUtil.sendMsgToOne(webSocketSession, JSONUtil.toJsonStr(msgObj));
+
+        List<Bid> bidList = bidRepository.getListByBwicid(bwicId);
+        bidList.forEach(bidInfo -> {
+            if(bidInfo.getAccountId().intValue() != bid.getAccountId().intValue()){
+                WebSocketSession webSocketSession1 = UserIdSessionManager.getSession(bidInfo.getAccountId().intValue());
+                JSONObject msgObj1 = new JSONObject();
+                msgObj1.put("msgType", WebSocketMsgType.NOTICE_RESULT_MSG.getCode());
+                msgObj1.put("result", "success");
+                msgObj1.put("msg", "抱歉，您此次拍卖竞拍失败！");
+                WebSocketMessageUtil.sendMsgToOne(webSocketSession1, JSONUtil.toJsonStr(msgObj1));
+            }
+        });
+        return bid;
+    }
+
+}
---
+++ bwic-svc/src/main/java/com/ebidding/bwic/repository/BwicRepository.java
@@ -0,0 +1,36 @@
+package com.ebidding.bwic.repository;
+
+import com.ebidding.bwic.api.BwicDTO;
+import com.ebidding.bwic.domain.Bwic;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository
+public interface BwicRepository extends JpaRepository<Bwic, Long> {
+    //SELECT * FROM BOND WHERE CUSIP = ?
+//    Optional<Bwic> findByCusip(String cusip);
+
+    //SELECT * FROM BOND WHERE BWIC_ID = ?
+    Optional<Bwic> findByBwicId(long bwicId);
+
+    List<Bwic> findAllByDueTimeAfterOrderByDueTimeAsc(Timestamp time);
+
+    List<Bwic> findAllByDueTimeBeforeOrderByDueTimeDesc(Timestamp time);
+
+    @Modifying  //告诉JPA这是一个update或者delete操作，可能会产生脏数据
+    @Query("UPDATE Bwic b SET b.bidCounts = b.bidCounts + 1 WHERE b.bwicId = :bwicId")
+    void incrementBidCount(@Param("bwicId") Long bwicId);
+
+
+
+}
