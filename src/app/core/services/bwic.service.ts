f176797e911814c54966beee6cb920ef1d18bb82
Zzz212zzZ <2120020201@qq.com>
2023-07-08 17:34:11 +0800

    admin代码主row

--- src/app/core/services/bwic.service.ts
+++ src/app/core/services/bwic.service.ts
@@ -1,12 +1,60 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {ParentItemData } from 'src/app/pages/trader-portal/admin/ongoing-table/ongoing-table.component';
+import { Observable,map } from 'rxjs';
+
+interface ServerResponseData {
+  bwicId: number;
+  bondId: string;
+  cusip: string;
+  issuer: string;
+  size: number;
+  startTime: string;
+  dueTime: string;
+  lastBidTime: string;
+  bidCounts: number;
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class BwicService {
-  
-  constructor(private http: HttpClient) {}
+
+  constructor(private http: HttpClient) { }
+
+
+  //---------------向后端请求ongoing的数据----------------
+   getOngoingBwics(): Observable<ParentItemData[]> {
+    const apiUrl = 'bwic/bwics/ongoing';
+    console.log("bwic.service.ts: getOngoingBwics(): apiUrl = " + apiUrl);
+    return this.http.get<ServerResponseData[]>(apiUrl).pipe(
+      map((data: ServerResponseData[]) => 
+        data.map(item => ({
+          bwic_id: item.bwicId,
+          bond_id: item.bondId,
+          cusip: item.cusip,
+          issuer: item.issuer,
+          size: item.size,
+          start_time: item.startTime,
+          due_time: item.dueTime,
+          last_bid_time: item.lastBidTime,
+          bid_counts: item.bidCounts,
+          expand: false,  // 默认设置为false
+          children: []
+        }))
+      )
+    );
+  }
+
+
+  //---------------向后端请求ongoing的数据----------------
+
+
+
+
+
+
 
   getBwics() {
     this.http
@@ -16,17 +64,17 @@ export class BwicService {
       });
   }
 
-  getBidding(value:string){
+  getBidding(value: string) {
     return this.http
       .get(`/api/bwics/${value}/bycusip`);
   }
 
-  getHistory(){
+  getHistory() {
     return this.http
       .get(`/api/bwics/history`).toPromise();
   }
 
-  setBids(model:{bwicId:string,price:string}){
-    return this.http.post('/bid/bids',model)
+  setBids(model: { bwicId: string, price: string }) {
+    return this.http.post('/bid/bids', model)
   }
 }
--- src/app/pages/trader-portal/admin/ongoing-table/ongoing-table.component.ts
+++ src/app/pages/trader-portal/admin/ongoing-table/ongoing-table.component.ts
@@ -1,23 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
+import { NzDrawerService } from 'ng-zorro-antd/drawer';
+import { DrawerContentComponent } from './drawer-content/drawer-content.component';
+import { BwicService } from 'src/app/core/services/bwic.service';
 
 
 
-
-interface ParentItemData {
-  bwic_id: number;
-  issuer: string;
-  size: number;
-  start_time: string;
-  due_time: string;
-  last_bid_time: string;
-  bid_counts: number;
-  expand: boolean;
-}
-
-
-interface ChildrenItemData {
+export interface BidRankItemData {
   bid_id: number;
   account_id: number;
   account_name: string;
@@ -27,50 +18,112 @@ interface ChildrenItemData {
 }
 
 
+
+export interface ParentItemData {
+  bwic_id: number;
+  bond_id: string;
+  cusip: string;
+  issuer: string;
+  size: number;
+  start_time: string;
+  due_time: string;
+  last_bid_time: string;
+  bid_counts: number;
+  expand: boolean;
+  children: BidRankItemData[];
+}
+
+
+
+
+
 @Component({
   selector: 'app-ongoing-table',
   standalone: true,
   imports: [
     CommonModule,
-    NzTableModule
+    NzTableModule,
+    NzDrawerModule
   ],
   templateUrl: './ongoing-table.component.html',
   styleUrls: ['./ongoing-table.component.less']
 })
 
 
-export class OngoingTableComponent implements OnInit {
+export class OngoingTableComponent {
+
+
+constructor(private drawerService: NzDrawerService, private bwicService: BwicService) {}
+
+  selectedRow: ParentItemData | null = null;
 
   @Input() data: any[] = [];
   listOfParentData: ParentItemData[] = [];
-  listOfChildrenData: ChildrenItemData[] = [];
-
-  
+  listOfBidRankData: BidRankItemData[] = [];
 
   ngOnInit(): void {
-    for (let i = 0; i < 3; ++i) {
-      this.listOfParentData.push({
-        bwic_id: i,
-        issuer: 'Issuer ' + i,
-        size: 500,
-        start_time: '2023-07-07 23:12:00',
-        due_time: '2023-07-08 23:12:00',
-        last_bid_time: '2023-07-09 23:12:00',
-        bid_counts: 20,
-        expand: false
-      });
-    }
-    for (let i = 0; i < 3; ++i) {
-      this.listOfChildrenData.push({
-        bid_id: i,
-        account_id: i,
-        account_name: 'Account ' + i,
-        price: 100,
-        ranking: i + 1,
-        time: '2023-07-07 23:12:00'
-      });
-    }
+    console.log("ongoing-table.component.ts: ngOnInit(): void");
+    this.getBwics();
+  }
+
+  getBwics(): void {
+    this.bwicService.getOngoingBwics().subscribe(data => this.listOfParentData = data);
+  }
+
+
+
+  onRowClick(data: ParentItemData): void {
+    this.selectedRow = data;
+    console.log('Row clicked: ', data);
+
+    const drawerRef = this.drawerService.create({
+      nzTitle: '详细信息',
+      nzPlacement: 'right',
+      nzWidth: '80%',  // 宽度设为屏幕的 80%
+      nzBodyStyle: {   // 自定义抽屉的样式
+        padding: '20px',
+        backgroundColor: '#f0f2f5', // 设置背景颜色为淡灰色
+      },
+      nzContent: DrawerContentComponent,
+      nzContentParams: {
+        data: [
+          {
+            rank: 1,
+            price: 10,
+            account_id: 'A001',
+            account_name: 'John Doe',
+            transaction_time: '2023-07-08 10:00:00'
+          },
+          {
+            rank: 2,
+            price: 20,
+            account_id: 'A002',
+            account_name: 'Jane Smith',
+            transaction_time: '2023-07-08 11:00:00'
+          },
+          {
+            rank: 3,
+            price: 15,
+            account_id: 'A003',
+            account_name: 'Bob Johnson',
+            transaction_time: '2023-07-08 09:00:00'
+          },
+          {
+            rank: 4,
+            price: 25,
+            account_id: 'A004',
+            account_name: 'Alice Williams',
+            transaction_time: '2023-07-08 12:00:00'
+          }
+        ]
+      }
+    });
   }
   
 
+
+  
+
+
 }
+
