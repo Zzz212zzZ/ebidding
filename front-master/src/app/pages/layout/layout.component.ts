fffad9eb18b756638df7c430f26b9fa5d4213be6
tyx <tianyx22379@126.com>
2023-07-12 11:04:25 +0800

    logout

--- front-master/src/app/pages/layout/layout.component.ts
+++ front-master/src/app/pages/layout/layout.component.ts
@@ -77,6 +77,6 @@ export class LayoutComponent {
 
   logout(): void{
     localStorage.clear();//清除localstorage
-    window.location.href = `http://localhost:${window.location.port}/login`;
+    window.location.href = `http://47.108.72.83:80`;
   }
 }
